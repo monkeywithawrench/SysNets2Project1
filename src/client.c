c037a588ac49f71155906a86e34117a00d2fc128
tct912 <tct5@students.uwf.edu>
2018-03-09 14:55:44 -0600

    Code check

--- src/client.c
+++ src/client.c
@@ -8,63 +8,65 @@
 #include <netdb.h>
 
 //The client
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
+
+	struct sockaddr_in server_address;//IPV4
+	struct hostent *server;//store info of host
 	int socket_hold, port, n;
-	struct sockaddr_in server_address;
-	struct hostent *server;
-	
 	char buffer[256];
+
 	//exe >> purpose >> port#
 	if (argc < 3){
 		fprintf(stderr,"Please provide a hostname and port number\n");
 		exit(0);
 	}
 
+	//Check for valid port 
 	port = atoi(argv[2]);
-   
-   /*if(portno < 60001 || portno > 60099){    //Check for valid port 
+        /*if(portno < 60001 || portno > 60099){    
 		fprintf(stderr,"Invalid port number");
 		exit(0);
 	}*/
 
-	socket_hold = socket(AF_INET, SOCK_STREAM, 0);
-
+	//Check for socket
+	socket_hold = socket(AF_INET, SOCK_STREAM, 0);//return file descriptor, else -1
 	if (socket_hold < 0 ){
 		fprintf(stderr,"Socket Fail");
       		exit(0);
 	}
-	server = gethostbyname(argv[1]);//server name
+
+	//Check hostname
+	server = gethostbyname(argv[1]);//server name, return hostent
 	if (server == NULL){
 		fprintf(stderr,"Host don't exist\n");
 		exit(0);
 	}
 	
-	//int addrSize = sizeof(server_address);
-	//char * addr = (char*) &server_address;
-	bzero((char*) &server_address,sizeof(server_address));//clear adress
-	server_address.sin_family = AF_INET;
-
-	bcopy((char*)server->h_addr,(char*) &server_address.sin_addr.s_addr,server->h_length);
+	memset((char*) &server_address,0,sizeof(server_address));
+	server_address.sin_family = AF_INET;//For internet, IP address, address family
+	memmove((char*) &server_address.sin_addr.s_addr,(char*)server->h_addr,server->h_length);
 	server_address.sin_port = htons(port);
 	
-	int checkConnect = connect(socket_hold,(struct sockaddr *)&server_address,sizeof(server_address));
+	//Check the connection
+	int checkConnect = connect(socket_hold,(struct sockaddr *)&server_address,sizeof(server_address));//(reference to socket by file descriptor,the specified address, address space of socket)
 	if ( checkConnect < 0){
 		fprintf(stderr,"Failed connectiopn");
       		exit(0);
 	}
+
+	/*Interface with the user*/
 	printf("Enter message sent to server: ");
-	bzero(buffer,256);
+	memset(buffer,0,256);
 	fgets(buffer,255,stdin);
-	
-	n = write(socket_hold,buffer,strlen(buffer));
-	if (n < 0)
-	{
+	n = write(socket_hold,buffer,strlen(buffer));//(reference to socket by file descriptor, the message written, write up to this length
+	//Check write success
+	if (n < 0){
 		fprintf(stderr,"Writing to socket fail");
       		exit(0);
 	}
-	bzero(buffer,256);
-	n = read(socket_hold,buffer,255);
+	memset(buffer,0,256);
+	//Check read success
+	n = read(socket_hold,buffer,255);//(reference to socket by file descriptor, the message read, read up to this length)
 	if (n < 0){
 		fprintf(stderr,"Reading from socket fail");
       		exit(0);
--- src/server.c
+++ src/server.c
@@ -8,48 +8,48 @@
 
 //The server
 int main(int argc, char *argv[]){
+	
+	struct sockaddr_in server_address, client_address;
 	int socket_hold, newsocket, port, client, n;
 	char buffer[256];
-	struct sockaddr_in server_address, client_address;
+
 	//exe >> port#
 	if (argc < 2){
 		fprintf(stderr,"You haven't provided a port number\n");
 		exit(1);
 	}
-
+	//Check socket success
 	socket_hold = socket(AF_INET, SOCK_STREAM, 0);
-
 	if (socket_hold < 0){
 		fprintf(stderr,"Opening socket failed");
       		exit(1);
 	}
 
-	bzero((char *) &server_address, sizeof(server_address));
-
+	//Start socketing process
+	memset((char *) &server_address,0,sizeof(server_address));
 	port = atoi(argv[1]);
-
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = INADDR_ANY;
 	server_address.sin_port = htons(port);
 
-	if (bind(socket_hold, (struct sockaddr *) &server_address,sizeof(server_address)) < 0){
+	//Check binding success
+	if (bind(socket_hold, (struct sockaddr *) &server_address,sizeof(server_address)) < 0){//the socket, its cast, the size
 		fprintf(stderr,"Binding failed");
      	 	exit(1);
 	}
 
 	while(1==1) { //while true (runs forever)
-		listen(socket_hold,5);
+		listen(socket_hold,5);//(reference to file descriptor, max queue
 		client = sizeof(client_address);
 
-		newsocket = accept(socket_hold,(struct sockaddr *) &client_address, &client);
+		newsocket = accept(socket_hold,(struct sockaddr *) &client_address, &client);//(file descriptor socketed-binded-listened,pointer to sockaddr struct, sizeof pointed struct)
 
 		if (newsocket < 0){
 			fprintf(stderr,"Accept failed");
          		exit(1);
 		}
 
-		bzero(buffer,256);
-
+		memset(buffer,0,256);
 		n = read(newsocket,buffer,255);
 
 		if (n <= 0){
@@ -57,12 +57,13 @@ int main(int argc, char *argv[]){
          		exit(1);
 		}
 		else {
-			char temp[strlen(buffer)];
-			strcpy(temp, buffer);
+			char temp[strlen(buffer)+1];
+			strcpy(temp, buffer);//placeholder for buffer in temp
 			printf("%s\n",buffer);
-			char *token;
-			char delim[] = " ";
+			char *token;		      
+			char delim[2] = " ";
 			token = strtok(temp, delim);  // first call returns pointer to first part of user_input separated by delim
+			/*TODO: seg fault, no space in string triggers it when sending a message*/
 			token = strtok(NULL, delim);  // every call with NULL uses saved user_input value and returns next substring
 			token++;
 			char filename[strlen(token)];
@@ -71,7 +72,7 @@ int main(int argc, char *argv[]){
 			//printf("Here is the message: %s\n",buffer);
 			//printf("%s\n",buffer);
 
-			n = write(newsocket,"Message is up, thanks",18);
+			n = write(newsocket,"Message is up, thanks\n",256);
 
 			if (n < 0){
 				fprintf(stderr,"Writing to socket fail");
