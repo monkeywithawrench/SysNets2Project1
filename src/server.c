d5736cc349be263d6d16e9ecfd4a3d730469e4db
Jeff <monkeybuilderman@gmail.com>
2018-03-17 03:59:12 -0500

    MASSIVE MASSIVE MASSIVE CHANGES! WE'RE MULTI-THREADED!
    May not work, but it should. This will probably fix a lot of issues when
    the server gets multiple requests at once

--- src/server.c
+++ src/server.c
@@ -46,120 +46,136 @@ int main(int argc, char *argv[]){
      	 	exit(1);
 	}
 
+	int pid; //MULTITHREADING, FK YEAH!!!
+	listen(socket_hold,5);//(reference to file descriptor, max queue
+	client = sizeof(client_address);
 	while(1==1) { //while true (runs forever)
-		listen(socket_hold,5);//(reference to file descriptor, max queue
-		client = sizeof(client_address);
+		//listen(socket_hold,5);//(reference to file descriptor, max queue
+		//client = sizeof(client_address);
 
 		newsocket = accept(socket_hold,(struct sockaddr *) &client_address, &client);//(file descriptor socketed-binded-listened,pointer to sockaddr struct, sizeof pointed struct)
 
 		if (newsocket < 0){
 			fprintf(stderr,"Accept failed");
-         		exit(1);
+			exit(1);
 		}
 
-		memset(buffer,0,BUFFER_MAX_SIZE);
-		n = read(newsocket,buffer,BUFFER_MAX_SIZE);
-
-		if (n <= 0){
-			fprintf(stderr,"Reading from socket fail");
-         		exit(1);
+		pid = fork(); //creating child process
+		if (pid < 0) { //something went wrong, pid is only <0 when a fork error occurs
+			fprintf(stderr,"ERROR on fork");
+			exit(1);
 		}
-		else {
-			char temp[strlen(buffer)+1];
-			strcpy(temp, buffer);//placeholder for buffer in temp
-			printf("%s\n",buffer);
-			char *token;		      
-			char delim[2] = " ";
-			token = strtok(temp, delim);  // first call returns pointer to first part of user_input separated by delim
-			/*TODO: seg fault, no space in string triggers it when sending a message*/
-			token = strtok(NULL, delim);  // every call with NULL uses saved user_input value and returns next substring
-			token++;
-			char filename[strlen(token)];
-			strcpy( filename, token); //filename now saved
-			strcpy(delim,"."); //going for the file extension now
-			strcpy(temp, filename);
-			token = strtok(temp, delim); //this part is file name without extension
-			token = strtok(NULL, delim); //YEEEEES, THIS IS THE GOOD STUFF!
-			char fileExtension[strlen(token)];
-			strcpy(fileExtension, token); //ALL YOUR TOKENS ARE BELONG TO MEEEE!
+
+		if (pid == 0) { //THIS IS NOW THE CHILD PROCESS! DO STUFFS!
+			close(socket_hold); //freeing this up, no longer needed
+			memset(buffer,0,BUFFER_MAX_SIZE);
+			n = read(newsocket,buffer,BUFFER_MAX_SIZE);
+
+			if (n <= 0){
+				fprintf(stderr,"Reading from socket fail");
+				exit(1);
+			}
+			else {
+				char temp[strlen(buffer)+1];
+				strcpy(temp, buffer);//placeholder for buffer in temp
+				printf("%s\n",buffer);
+				char *token;
+				char delim[2] = " ";
+				token = strtok(temp, delim);  // first call returns pointer to first part of user_input separated by delim
+				/*TODO: seg fault, no space in string triggers it when sending a message*/
+				token = strtok(NULL, delim);  // every call with NULL uses saved user_input value and returns next substring
+				token++;
+				char filename[strlen(token)];
+				strcpy( filename, token); //filename now saved
+				strcpy(delim,"."); //going for the file extension now
+				strcpy(temp, filename);
+				token = strtok(temp, delim); //this part is file name without extension
+				token = strtok(NULL, delim); //YEEEEES, THIS IS THE GOOD STUFF!
+				char fileExtension[strlen(token)];
+				strcpy(fileExtension, token); //ALL YOUR TOKENS ARE BELONG TO MEEEE!
 
 
-			if(strlen(filename)>0 && strlen(fileExtension)>0) {
-				char *fileLocation;
-				asprintf(&fileLocation, "./%s", filename);
-				if(access(fileLocation, R_OK) != -1) {  //F_OK checks if file exists, R_OK checks if file can be read
-					//file exists
+				if(strlen(filename)>0 && strlen(fileExtension)>0) {
+					char *fileLocation;
+					asprintf(&fileLocation, "./%s", filename);
+					if(access(fileLocation, R_OK) != -1) {  //F_OK checks if file exists, R_OK checks if file can be read
+						//file exists
 
-					char *postrequest;
-					//asprintf(&postrequest, "POST /%s HTTP/1.1\n", filename);
-					asprintf(&postrequest, "HTTP/1.1 200 OK\n");
-					//asprintf(&postrequest, "%sHost: notarealaddress\n", postrequest);
-					//asprintf(&postrequest, "%sConnection: keep-alive\n");
-					//asprintf(&postrequest, "%sConnection: close\n", postrequest);
-					//asprintf(&postrequest, "%sContent-Length: 44\n", postrequest); //TODO set this to actual file size
-					//asprintf(&postrequest, "%sCache-Control: no-cache\n", postrequest);
-					//asprintf(&postrequest, "%sOrigin: Server program info\n", postrequest);
-					//asprintf(&postrequest, "%sUser-Agent: Server machine info\n", postrequest);
-					//	asprintf(&postrequest, "%sContent-Type: text/html\n", postrequest);
-					//	asprintf(&postrequest, "%s\n<html><body><h1>It works!</h1></body></html>\n", postrequest);
+						char *postrequest;
+						//asprintf(&postrequest, "POST /%s HTTP/1.1\n", filename);
+						asprintf(&postrequest, "HTTP/1.1 200 OK\n");
+						//asprintf(&postrequest, "%sHost: notarealaddress\n", postrequest);
+						//asprintf(&postrequest, "%sConnection: keep-alive\n");
+						//asprintf(&postrequest, "%sConnection: close\n", postrequest);
+						//asprintf(&postrequest, "%sContent-Length: 44\n", postrequest); //TODO set this to actual file size
+						//asprintf(&postrequest, "%sCache-Control: no-cache\n", postrequest);
+						//asprintf(&postrequest, "%sOrigin: Server program info\n", postrequest);
+						//asprintf(&postrequest, "%sUser-Agent: Server machine info\n", postrequest);
+						//	asprintf(&postrequest, "%sContent-Type: text/html\n", postrequest);
+						//	asprintf(&postrequest, "%s\n<html><body><h1>It works!</h1></body></html>\n", postrequest);
 
 
-					long fileSize;
+						long fileSize;
 
-					if(strcmp(fileExtension, "html")==0 || strcmp(fileExtension, "htm")==0 || strcmp(fileExtension, "txt")==0 ) {
-						char *fileContents = readFile(fileLocation, "r"); //"r" to read the file as text
-						asprintf(&postrequest, "%sContent-Length: %d\n", postrequest, strlen(fileContents)); //pls work
-						asprintf(&postrequest, "%sContent-Type: text/html\n", postrequest);
-						asprintf(&postrequest, "%s\n%s", postrequest, fileContents); //append file contents to postrequest
-						free(fileContents); //ALWAYS FREE YOUR MALLOCS WHEN DONE, MKAY?!
-						n = write(newsocket,postrequest, strlen(postrequest));
+						if(strcmp(fileExtension, "html")==0 || strcmp(fileExtension, "htm")==0 || strcmp(fileExtension, "txt")==0 ) {
+							char *fileContents = readFile(fileLocation, "r"); //"r" to read the file as text
+							asprintf(&postrequest, "%sContent-Length: %d\n", postrequest, strlen(fileContents)); //pls work
+							asprintf(&postrequest, "%sContent-Type: text/html\n", postrequest);
+							asprintf(&postrequest, "%s\n%s", postrequest, fileContents); //append file contents to postrequest
+							free(fileContents); //ALWAYS FREE YOUR MALLOCS WHEN DONE, MKAY?!
+							n = write(newsocket,postrequest, strlen(postrequest));
+						}
+						else {
+							char *fileContents = readFile(fileLocation, "rb"); //"rb" to read the file as text
+							fileSize = getFileSize(fileLocation);
+							asprintf(&postrequest, "%sContent-Length: %ld\n", postrequest, getFileSize(fileLocation)); //pls work
+							fprintf(stderr, "FileSize: %ld\n", fileSize);
+							//fprintf(stderr, "%s", fileContents);
+
+							if(strcmp(fileExtension, "jpg")==0 || strcmp(fileExtension, "jpeg")==0) { //IMAGES
+								asprintf(&postrequest, "%sContent-Type: image/jpeg\n\n", postrequest);
+							}
+							else if(strcmp(fileExtension, "png")==0) { //IMAGES
+								asprintf(&postrequest, "%sContent-Type: image/png\n\n", postrequest);
+							}
+							else if(strcmp(fileExtension, "gif")==0) { //IMAGES
+								asprintf(&postrequest, "%sContent-Type: image/gif\n\n", postrequest);
+							}
+							else if(strcmp(fileExtension, "ico")==0) { //IMAGES
+								asprintf(&postrequest, "%sContent-Type: image/icon\n\n", postrequest);
+							}
+
+							long headerSize = strlen(postrequest);
+							//asprintf(&postrequest, "%s%s", postrequest, fileContents); //append file contents to postrequest
+							char *response = malloc(headerSize + fileSize);
+							memcpy(response, (void *)postrequest, headerSize); //copy the header into our new memory chunk
+							memcpy(response+headerSize, (void *)fileContents, fileSize); //append the file to our memory chunk
+
+							//fprintf(stdout, "\n\nRESPONSE:\n%s", response);
+							//n = write(newsocket,postrequest, headerSize + fileSize);
+							n = write(newsocket, response, headerSize + fileSize);
+							free(fileContents); //ALWAYS FREE YOUR MALLOCS WHEN DONE, MKAY?!
+							free(response); //free the response too
+						}
 					}
 					else {
-						char *fileContents = readFile(fileLocation, "rb"); //"rb" to read the file as text
-						fileSize = getFileSize(fileLocation);
-						asprintf(&postrequest, "%sContent-Length: %ld\n", postrequest, getFileSize(fileLocation)); //pls work
-						fprintf(stderr, "FileSize: %ld\n", fileSize);
-						//fprintf(stderr, "%s", fileContents);
-
-						if(strcmp(fileExtension, "jpg")==0 || strcmp(fileExtension, "jpeg")==0) { //IMAGES
-							asprintf(&postrequest, "%sContent-Type: image/jpeg\n\n", postrequest);
-						}
-						else if(strcmp(fileExtension, "png")==0) { //IMAGES
-							asprintf(&postrequest, "%sContent-Type: image/png\n\n", postrequest);
-						}
-						else if(strcmp(fileExtension, "gif")==0) { //IMAGES
-							asprintf(&postrequest, "%sContent-Type: image/gif\n\n", postrequest);
-						}
-						else if(strcmp(fileExtension, "ico")==0) { //IMAGES
-							asprintf(&postrequest, "%sContent-Type: image/icon\n\n", postrequest);
-						}
-
-						long headerSize = strlen(postrequest);
-						//asprintf(&postrequest, "%s%s", postrequest, fileContents); //append file contents to postrequest
-						char *response = malloc(headerSize + fileSize);
-						memcpy(response, (void *)postrequest, headerSize); //copy the header into our new memory chunk
-						memcpy(response+headerSize, (void *)fileContents, fileSize); //append the file to our memory chunk
-
-						//fprintf(stdout, "\n\nRESPONSE:\n%s", response);
-						//n = write(newsocket,postrequest, headerSize + fileSize);
-						n = write(newsocket, response, headerSize + fileSize);
-						free(fileContents); //ALWAYS FREE YOUR MALLOCS WHEN DONE, MKAY?!
-						free(response); //free the response too
+						char *postrequest;
+						asprintf(&postrequest, "HTTP/1.1 404 Not Found\n");
+						//TODO rest of 404 error response
 					}
 				}
-				else {
-					char *postrequest;
-					asprintf(&postrequest, "HTTP/1.1 404 Not Found\n");
-					//TODO rest of 404 error response
+
+				//n = write(newsocket,"Message is up, thanks\n",BUFFER_MAX_SIZE);
+
+				if (n < 0){
+					fprintf(stderr,"Writing to socket fail");
+					exit(1);
 				}
 			}
-
-			//n = write(newsocket,"Message is up, thanks\n",BUFFER_MAX_SIZE);
-
-			if (n < 0){
-				fprintf(stderr,"Writing to socket fail");
-            			exit(1);
-			}
+			exit(0); //closing this thread
+		}
+		else {
+			close(newsocket); //closing this
 		}
 	}
 	return 0;
